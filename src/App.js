9a5d454225c5aec62e150d7af2d4dd92d7cf3c85
busemzglu <busemzglu@gmail.com>
2021-04-10 21:34:12 +0300

    Route added.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Reports from "./pages/Reports";
+import Products from "./pages/Products";
+import Support from "./pages/Support";
+import Messages from "./pages/Messages";
 function App() {
   return (
     <>
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" />
+          <Route path="/" exact component={Home} />
+          <Route path="/reports" component={Reports} />
+          <Route path="/products" component={Products} />
+          <Route path="/messages" component={Messages} />
+          <Route path="/support" component={Support} />
         </Switch>
       </Router>
     </>
